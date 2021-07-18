a7dbecc8c95c63cb5ac3d42d8a3fcf0a947cf89c
Magic <gaorock530@hotmail.com>
2021-07-18 23:42:57 +0800

    update

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -65,6 +65,115 @@ const links = [
   ['https://deepdao.io/', 'https://republic.co/crypto', 'http://musenetwork.org/', 'https://www.alpha5.io/#/', 'https://mintable.app/'],
 ]
 
+const more = {
+  logo1: {
+    l1: {
+      t: 'LedgerX',
+      en: 'Leading investor in LedgerX, which aims to become the most important US regulated exchange for bitcoin derivatives.',
+      zh: '全球领先的受美国CFTC监管的数字资产期货和期权交易所'
+    },
+    l2: {
+      t: 'Circle',
+      en: 'The world\'s leading payments and treasury infrastructure service provider and creator of the fastest growing full reserve stablecoin in the world (USDC).',
+      zh: '全球领先的支付服务提供商，以及全球增长最快的稳定币USDC的发行方'
+    },
+    l3: {
+      t: 'Amber Group',
+      en: 'A leading global encrypted financial intelligent service platform.',
+      zh: '业内领先的全球化加密金融智能服务提供商'
+    },
+    l4: {
+      t: 'Matrix',
+      en: 'The most secure and compliant global virtual asset exchange in the Middle East.',
+      zh: '中东地区最安全、合规的全球数字资产交易平台'
+    }
+  },
+  logo2: {
+    l1: {
+      t: 'Acala',
+      en: 'The all-in-one DeFi hub of Polkadot',
+      zh: 'Polkadot一站式DeFi中心'
+    },
+    l2: {
+      t: 'Astar Network (Previously Plasm)',
+      en: 'Polkadot native dApp hub supporting EVM, WebAssembly and Layer2. ',
+      zh: 'Polkadot上支持以太坊虚拟机、WebAssembly以及Layer 2的原生dApp集线器'
+    },
+    l3: {
+      t: 'Manta Network',
+      en: 'An interoperable privacy protocol for the DeFi stack built on Substrate.',
+      zh: '基于 Substrate 构建的DeFi堆栈互操作隐私协议'
+    },
+    l4: {
+      t: 'Reef Finance',
+      en: 'A cross-chain DeFi operating system, smart liquidity aggregator and yield engine powered by Polkadot.',
+      zh: '由Polkadot赋能的跨链 DeFi 操作系统、智能流动性聚合器和收益引擎'
+    },
+    l5: {
+      t: 'HydraDX',
+      en: 'The multi-headed liquidity omni pool built on Substrate.',
+      zh: '基于 Substrate 构建的多头流动性综合池'
+    },
+    l6: {
+      t: 'Equilibrium',
+      en: 'The first interoperable DeFi conglomerate on Polkadot comprised of a money market and a cross-chain DEX.',
+      zh: 'Polkadot首个可互操作的由做市商和跨链DEX组成的DeFi协议'
+    },
+    l7: {
+      t: 'Unique',
+      en: 'An NFT chain for Kusama and Polkadot.',
+      zh: '基于Kusama和Polkadot生态、专注于NFT的区块链'
+    },
+    l8: {
+      t: 'Bit.Country',
+      en: 'A metaverse and NFT portal on Polkadot.',
+      zh: '建立在Polkadot生态上的“元宇宙”和NFT世界传送门'
+    }
+  },
+  logo3: {
+    l1: {
+      t: 'Solana',
+      en: 'A high-performance blockchain supporting builders around the world creating crypto apps.',
+      zh: '高性能的支持全球加密应用的区块链平台'
+    },
+    l2: {
+      t: 'NEAR',
+      en: 'An open-source platform that accelerates the development of decentralized applications.',
+      zh: '加速去中心化应用程序开发的开源平台'
+    },
+    l3: {
+      t: 'Avalanche',
+      en: 'A highly-scalable, open-source platform for launching decentralized finance applications.',
+      zh: '支持去中心化金融应用的高度可扩展的开源平台'
+    },
+    l4: {
+      t: 'Swarm',
+      en: 'A decentralized storage and communication system of the Ethereum Web3.0 stack.',
+      zh: '以太坊Web3.0堆栈的分布式存储和通信网络'
+    },
+    l5: {
+      t: 'Chia Network',
+      en: 'A blockchain and smart transaction platform focused on energy efficiency.',
+      zh: '专注于能源效率的区块链和智能交易平台'
+    },
+    l6: {
+      t: 'RNDR',
+      en: 'The first network to transform the power of GPU compute into a decentralized economy of connected 3D assets.',
+      zh: '首个将GPU计算能力转换为连接3D资产的去中心化经济网络'
+    },
+    l7: {
+      t: 'NuCypher',
+      en: 'Cryptographic infrastructure for privacy-preserving applications.',
+      zh: '隐私保护应用程序的加密基础设施'
+    },
+    l8: {
+      t: 'Dao Maker',
+      en: 'A DeFi platform for retail ventures looking to invest in equity and tokens.',
+      zh: '面向寻求股票和代币投资的零售企业的DeFi平台'
+    }
+  }
+}
+
 export default function Portfolio () {
   const [{language}] = useContext(ThemeContext);
 
@@ -100,10 +209,10 @@ export default function Portfolio () {
         </div>
         <div className="mid">
           <div className="limit logos">
-            <a href={links[0][0]} target="_blink"><img alt="" src="/images/portfolio/11.png" /></a>
-            <a href={links[0][1]} target="_blink"><img alt="" src="/images/portfolio/12.png" /></a>
-            <a href={links[0][2]} target="_blink"><img alt="" src="/images/portfolio/13.png" /></a>
-            <a href={links[0][3]} target="_blink"><img alt="" src="/images/portfolio/14.png" /></a>
+            <a href={links[0][0]} target="_blink"><img alt="" src="/images/portfolio/11.png" /><div className="more"><h3>{more.logo1.l1.t}</h3><p>{more.logo1.l1[language]}</p></div></a>
+            <a href={links[0][1]} target="_blink"><img alt="" src="/images/portfolio/12.png" /><div className="more"><h3>{more.logo1.l2.t}</h3><p>{more.logo1.l2[language]}</p></div></a>
+            <a href={links[0][2]} target="_blink"><img alt="" src="/images/portfolio/13.png" /><div className="more"><h3>{more.logo1.l3.t}</h3><p>{more.logo1.l3[language]}</p></div></a>
+            <a href={links[0][3]} target="_blink"><img alt="" src="/images/portfolio/14.png" /><div className="more"><h3>{more.logo1.l4.t}</h3><p>{more.logo1.l4[language]}</p></div></a>
           </div>
         </div>
         <div className="limit grid">
@@ -135,16 +244,16 @@ export default function Portfolio () {
         </div>
         <div className="mid">
           <div className="limit logos">
-            <a href={links[5][0]} target="_blink"><img alt="" src="/images/portfolio/61.png" /></a>
-            <a href={links[5][1]} target="_blink"><img alt="" src="/images/portfolio/62.png" /></a>
-            <a href={links[5][2]} target="_blink"><img alt="" src="/images/portfolio/63.png" /></a>
-            <a href={links[5][3]} target="_blink"><img alt="" src="/images/portfolio/64.png" /></a>
+            <a href={links[5][0]} target="_blink"><img alt="" src="/images/portfolio/61.png" /><div className="more"><h3>{more.logo2.l1.t}</h3><p>{more.logo2.l1[language]}</p></div></a>
+            <a href={links[5][1]} target="_blink"><img alt="" src="/images/portfolio/62.png" /><div className="more"><h3>{more.logo2.l2.t}</h3><p>{more.logo2.l2[language]}</p></div></a>
+            <a href={links[5][2]} target="_blink"><img alt="" src="/images/portfolio/63.png" /><div className="more"><h3>{more.logo2.l3.t}</h3><p>{more.logo2.l3[language]}</p></div></a>
+            <a href={links[5][3]} target="_blink"><img alt="" src="/images/portfolio/64.png" /><div className="more"><h3>{more.logo2.l4.t}</h3><p>{more.logo2.l4[language]}</p></div></a>
           </div>
           <div className="limit logos">
-            <a href={links[6][0]} target="_blink"><img alt="" src="/images/portfolio/71.png" /></a>
-            <a href={links[6][1]} target="_blink"><img alt="" src="/images/portfolio/72.png" /></a>
-            <a href={links[6][2]} target="_blink"><img alt="" src="/images/portfolio/73.png" /></a>
-            <a href={links[6][3]} target="_blink"><img alt="" src="/images/portfolio/74.png" /></a>
+            <a href={links[6][0]} target="_blink"><img alt="" src="/images/portfolio/71.png" /><div className="more"><h3>{more.logo2.l5.t}</h3><p>{more.logo2.l5[language]}</p></div></a>
+            <a href={links[6][1]} target="_blink"><img alt="" src="/images/portfolio/72.png" /><div className="more"><h3>{more.logo2.l6.t}</h3><p>{more.logo2.l6[language]}</p></div></a>
+            <a href={links[6][2]} target="_blink"><img alt="" src="/images/portfolio/73.png" /><div className="more"><h3>{more.logo2.l7.t}</h3><p>{more.logo2.l7[language]}</p></div></a>
+            <a href={links[6][3]} target="_blink"><img alt="" src="/images/portfolio/74.png" /><div className="more"><h3>{more.logo2.l8.t}</h3><p>{more.logo2.l8[language]}</p></div></a>
           </div>
         </div>
         <div className="limit grid">
@@ -178,16 +287,16 @@ export default function Portfolio () {
         </div>
         <div className="mid">
           <div className="limit logos">
-            <a href={links[11][0]} target="_blink"><img alt="" src="/images/portfolio/121.png" /></a>
-            <a href={links[11][1]} target="_blink"><img alt="" src="/images/portfolio/122.png" /></a>
-            <a href={links[11][2]} target="_blink"><img alt="" src="/images/portfolio/123.png" /></a>
-            <a href={links[11][3]} target="_blink"><img alt="" src="/images/portfolio/124.png" /></a>
+            <a href={links[11][0]} target="_blink"><img alt="" src="/images/portfolio/121.png" /><div className="more"><h3>{more.logo3.l1.t}</h3><p>{more.logo3.l1[language]}</p></div></a>
+            <a href={links[11][1]} target="_blink"><img alt="" src="/images/portfolio/122.png" /><div className="more"><h3>{more.logo3.l2.t}</h3><p>{more.logo3.l2[language]}</p></div></a>
+            <a href={links[11][2]} target="_blink"><img alt="" src="/images/portfolio/123.png" /><div className="more"><h3>{more.logo3.l3.t}</h3><p>{more.logo3.l3[language]}</p></div></a>
+            <a href={links[11][3]} target="_blink"><img alt="" src="/images/portfolio/124.png" /><div className="more"><h3>{more.logo3.l4.t}</h3><p>{more.logo3.l4[language]}</p></div></a>
           </div>
           <div className="limit logos">
-            <a href={links[12][0]} target="_blink"><img alt="" src="/images/portfolio/131.png" /></a>
-            <a href={links[12][1]} target="_blink"><img alt="" src="/images/portfolio/132.png" /></a>
-            <a href={links[12][2]} target="_blink"><img alt="" src="/images/portfolio/133.png" /></a>
-            <a href={links[12][3]} target="_blink"><img alt="" src="/images/portfolio/134.png" /></a>
+            <a href={links[12][0]} target="_blink"><img alt="" src="/images/portfolio/131.png" /><div className="more"><h3>{more.logo3.l5.t}</h3><p>{more.logo3.l5[language]}</p></div></a>
+            <a href={links[12][1]} target="_blink"><img alt="" src="/images/portfolio/132.png" /><div className="more"><h3>{more.logo3.l6.t}</h3><p>{more.logo3.l6[language]}</p></div></a>
+            <a href={links[12][2]} target="_blink"><img alt="" src="/images/portfolio/133.png" /><div className="more"><h3>{more.logo3.l7.t}</h3><p>{more.logo3.l7[language]}</p></div></a>
+            <a href={links[12][3]} target="_blink"><img alt="" src="/images/portfolio/134.png" /><div className="more"><h3>{more.logo3.l8.t}</h3><p>{more.logo3.l8[language]}</p></div></a>
           </div>
         </div>
         <div className="limit grid">
